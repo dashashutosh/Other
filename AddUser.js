f35e21bc7e7826faa28e13f7438487bc61251b17
Ashutosh Dash <53340217+dashashutosh@users.noreply.github.com>
2025-04-01 23:28:27 +0530

    Add files via upload

---
+++ AddUser.js
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../redux/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { Container, TextField, Button, Grid, Typography, Tabs, Tab, Box, InputAdornment, AppBar, Toolbar } from '@mui/material';
+import { AccountCircle, Email, Phone, Language, LocationCity, Business, Add } from '@mui/icons-material';
+
+function AddUser() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [tabIndex, setTabIndex] = useState(0);
+
+    const [userInfo, setUserInfo] = useState({
+        name: '',
+        username: '',
+        email: '',
+        phone: '',
+        website: '',
+        address: { city: '', street: '', suite: '', zipcode: '' },
+        company: { name: '', catchPhrase: '', bs: '' }
+    });
+
+    const handleChange = (e) => {
+        setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+    };
+
+    const handleAddressChange = (e) => {
+        setUserInfo({ ...userInfo, address: { ...userInfo.address, [e.target.name]: e.target.value } });
+    };
+
+    const handleCompanyChange = (e) => {
+        setUserInfo({ ...userInfo, company: { ...userInfo.company, [e.target.name]: e.target.value } });
+    };
+
+    const addNewUser = () => {
+        dispatch(addUser(userInfo)).then(() => navigate('/'));
+    };
+
+    return (
+        <>
+            <AppBar position="static" sx={{ backgroundColor: "#1976d2" }}>
+                <Toolbar sx={{ display: "flex", justifyContent: "center" }}>
+                    <Typography variant="h4" sx={{ fontWeight: "normal" }}>
+                        Employee Management System
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <Container maxWidth="md" sx={{ mt: 4 }}>
+
+                <Tabs value={tabIndex} onChange={(_, newValue) => setTabIndex(newValue)} centered data-testid="tabs">
+                    <Tab label="Basic Details" data-testid="tab-basic-details" />
+                    <Tab label="Address Details" data-testid="tab-address-details" />
+                    <Tab label="Company Details" data-testid="tab-company-details" />
+                </Tabs>
+
+                <Box sx={{ mt: 3 }}>
+                    {tabIndex === 0 && (
+                        <Grid container spacing={2} data-testid="basic-details-form">
+                            {[{ label: 'Name', name: 'name', icon: <AccountCircle /> },
+                              { label: 'Username', name: 'username', icon: <AccountCircle /> },
+                              { label: 'Email', name: 'email', icon: <Email /> },
+                              { label: 'Phone', name: 'phone', icon: <Phone /> },
+                              { label: 'Website', name: 'website', icon: <Language /> }].map((field) => (
+                                <Grid item xs={12} md={6} key={field.name}>
+                                    <TextField
+                                        fullWidth
+                                        label={field.label}
+                                        name={field.name}
+                                        value={userInfo[field.name]}
+                                        onChange={handleChange}
+                                        InputProps={{ startAdornment: <InputAdornment position="start">{field.icon}</InputAdornment> }}
+                                        data-testid={`input-${field.name}`}
+                                    />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    )}
+
+                    {tabIndex === 1 && (
+                        <Grid container spacing={2} data-testid="address-details-form">
+                            {[{ label: 'City', name: 'city' }, { label: 'Street', name: 'street' },
+                              { label: 'Suite', name: 'suite' }, { label: 'Zipcode', name: 'zipcode' }].map((field) => (
+                                <Grid item xs={12} md={6} key={field.name}>
+                                    <TextField
+                                        fullWidth
+                                        label={field.label}
+                                        name={field.name}
+                                        value={userInfo.address[field.name]}
+                                        onChange={handleAddressChange}
+                                        InputProps={{ startAdornment: <InputAdornment position="start"><LocationCity /></InputAdornment> }}
+                                        data-testid={`input-address-${field.name}`}
+                                    />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    )}
+
+                    {tabIndex === 2 && (
+                        <Grid container spacing={2} data-testid="company-details-form">
+                            {[{ label: 'Company Name', name: 'name' },
+                              { label: 'Catch Phrase', name: 'catchPhrase' },
+                              { label: 'Business Strategy', name: 'bs' }].map((field) => (
+                                <Grid item xs={12} md={6} key={field.name}>
+                                    <TextField
+                                        fullWidth
+                                        label={field.label}
+                                        name={field.name}
+                                        value={userInfo.company[field.name]}
+                                        onChange={handleCompanyChange}
+                                        InputProps={{ startAdornment: <InputAdornment position="start"><Business /></InputAdornment> }}
+                                        data-testid={`input-company-${field.name}`}
+                                    />
+                                </Grid>
+                            ))}
+                        </Grid>
+                    )}
+                </Box>
+
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 3 }}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        startIcon={<Add />}
+                        onClick={addNewUser}
+                        data-testid="submit-user-button"
+                    >
+                        Add New User
+                    </Button>
+                </Box>
+            </Container>
+        </>
+    );
+}
+
+export default AddUser;
